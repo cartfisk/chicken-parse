564e89c33c907391beb05b7de14bbc4164830484
Carter Konz <carter.konz@gmail.com>
2016-01-30 13:23:35 -0600

    Initial commit. Did introcductory research, decided on Python and set up a virtualenvwrapper for the project. Currently learning how to implement unit testing in Python.

---
+++ chickenParse.py
@@ -0,0 +1,23 @@
+"""Twitter parser"""
+import sys
+
+URL = " "
+
+def main():
+    """Main entry point for the script."""
+    pass
+
+def get_mentions(url):
+    """Return the mentions from an individual tweet."""
+    pass
+
+def get_hashtags(url):
+    """Return the hastags from an individual tweet."""
+    pass
+
+def get_urls(url):
+    """Return the URLS from an individual tweet."""
+    pass
+
+if __name__ == '__main__':
+    sys.exit(main())
